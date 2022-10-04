853c90f7c8b12a2f0962f3ff360b4c73a5e1b2f1
jongkeelee <38681485+jongkeelee@users.noreply.github.com>
2022-10-04 19:06:02 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,37 +1,16 @@
-let profile_name, proflie_img, profile_avatar, intro_main, intro_sub;
-
-window.onload = function(){
-  init();
-  SetProfile();
-  Blog_Write_Post();
-}
-
-function SetProfile(){
-  document.title =  "Nickname";
-  profile_name.innerHTML = "Nicname";
-  profile_avatar.src = "./avatar.gif";
-  intro_main.innerHTML = "Title_Intro_Title";
-  intro_sub.innerHTML = "Photo booth cold-pressed air plant iceland actually single-origin coffee. Tacos kitsch tattooed, trust fund vaporware woke letterpress messenger bag photo booth viral. Vice 8-bit live-edge, iceland scenester next level affogato heirloom banh mi fingerstache you probably haven't heard of them schlitz fashion axe pok pok helvetica. Knausgaard post-ironic ramps mustache lo-fi. Knausgaard subway tile vegan banh mi hoodie VHS wayfarers direct trade williamsburg raclette schlitz enamel pin.";
-}
-function init(){
-  profile_name = document.querySelector('#nickname');
-  profile_avatar = document.querySelector('#profile_avatar');
-  intro_main = document.querySelector('#intro_main');
-  intro_sub = document.querySelector('#intro_sub');
-}
-
-function Blog_Write_Post(){
-  var Blog_Post_Info = document.getElementById('blog');
-  var Dummy_Text = `
-  <div class="column-xs-12 column-sm-6 portfolio-item">
-  <a href="#">
-    <figure>
-      <img src="aa">
-      <figcaption>
-        <h3>Project Title</h3>
-        <p style="margin-top: 10px;"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>    
-      </figcaption>
-    </figure>
-  </a>`;
-  Blog_Post_Info.innerHTML += Dummy_Text;
+function menu(idx) {
+    var ActiveIdx = [];
+    ActiveIdx[idx] = "active";
+    document.write(`<div class="header">
+    <ul>
+        <li style="float:left"><a href="./index.html">Jongkee Lee | 이종기</a></li>
+        <li style="float:right"><a href="https://www.linkedin.com/in/jongkee-lee-85845765"><i class="fa-brands fa-linkedin fa-xl"> </i></a></li>
+        <li style="float:right"><a href="https://www.instagram.com/jongkee.lee/"><i class="fa-brands fa-instagram fa-xl"></i></a></li>
+        <li style="float:right"><a class="`+ ActiveIdx[4] + `"href="./patent.html">PATENT</a></li>
+        <li style="float:right"><a class="`+ ActiveIdx[3] + `"href="./award.html">AWARD</a></li>
+        <li style="float:right"><a class="`+ ActiveIdx[2] + `"href="./design.html">DESIGN</a></li>
+        <li style="float:right"><a class="`+ ActiveIdx[1] + `"href="./family.html">FAMILY</a></li>
+        <li style="float:right"><a class="`+ ActiveIdx[0] + `"href="./about.html">ABOUT</a></li>    
+    </ul>
+</div>`);
 }
